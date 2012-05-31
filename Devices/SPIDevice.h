0a5a14035ec2c44135263681faf973fa7b4900aa
Phillip Johnston <phillip.johnston@gatech.edu>
2012-05-31 15:27:14 -0400

    Extracted common functionality from DAC and Quadrature Decoder classes in order to use a master SPIDevice class.

---
+++ Devices/SPIDevice.h
@@ -0,0 +1,42 @@
+/*
+* SPI Device Class
+* Created by Phillip Johnston
+* 31 May 2012
+*
+* This class was created to allow common function calls
+* to be encapsulated in a base SPIDevice Class
+*
+*/
+
+#ifndef SPI_DEVICE_H
+#define SPI_DEVICE_H
+
+#include <stdint.h>
+
+/********************
+* Define Statements *
+********************/
+//#define SPIDEV_DEBUG
+
+/********************
+* Class Declaration * 
+********************/
+class SPIDevice
+{
+public:
+	SPIDevice();
+	~SPIDevice();
+	void setCSPin(uint8_t cs_pin);
+	virtual void setupSPI();
+	uint8_t getChipSelectPin();
+	inline uint32_t getLastTransmissionResult(void);
+
+protected:
+	uint8_t CSpin;
+	inline void _enableChipSelect();
+	inline void _disableChipSelect();
+	void _send(uint8_t a, uint8_t b, uint8_t c);
+	uint32_t _transfer(uint8_t a, uint8_t b, uint8_t c);
+};
+
+#endif //SPI_DEVICE_H
