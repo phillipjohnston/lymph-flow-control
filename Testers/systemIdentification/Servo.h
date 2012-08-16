557b3395395b0f1cb358dbacc0afceb8b936b71d
Phillip Johnston <phillip.johnston@gatech.edu>
2012-08-16 14:23:57 -0400

    Updated the servo library used by the tester to match the new library version

--- Testers/systemIdentification/Servo.h
+++ Testers/systemIdentification/Servo.h
@@ -11,20 +11,23 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+#include <stdint.h>
+
 /********************
 * Class Declaration * 
 ********************/
 class Servo
 {
     public:
-        Servo(int chipSelect, int dacSelect);
+        Servo(uint16_t quadDecCS, uint16_t dacCS, uint8_t dac);
         void init(void);
         double position(void);
         void move(double volts);
 
     private:
         void _ls7366rConfig(void);
-        void _dacConfig(void);
+        void _dacConfig(uint8_t power_setting);
+        uint8_t selected_dac;
 };
 
 #endif //SERVO_H
